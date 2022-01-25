e56c187019464a724a3f1adb972b9c1e53583a5f
shittake <ericleowyuquan@gmail.com>
2022-01-25 22:56:28 +0800

    Initialize Interactor.java file which preliminarily interacts with the user

--- src/main/java/Interactor.java
+++ src/main/java/Interactor.java
@@ -1,2 +1,10 @@
-package main.java;public class Interactor {
+package main.java;
+import java.util.Scanner;
+
+public class Interactor {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(n * 4);
+    }
 }
