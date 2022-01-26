40b9692a3d80eaca56af54e13a92ed722a5cbf0c
shittake <ericleowyuquan@gmail.com>
2022-01-26 11:35:56 +0800

    Update Interactor.java to ask for user's name

--- src/main/java/Interactor.java
+++ src/main/java/Interactor.java
@@ -2,9 +2,27 @@ package main.java;
 import java.util.Scanner;
 
 public class Interactor {
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        System.out.println(n * 4);
+        welcome(scan);
+    }
+
+    public static void welcome(Scanner scan) {
+        System.out.println("Welcome to PlanningBot!");
+        System.out.println("Type Y (yes) or N (no) when answering any yes/no questions.");
+        System.out.print("Please enter your name: ");
+
+        String name = scan.nextLine();
+        System.out.println("Hello " + name + ". Is this correct?");
+
+        String reply = scan.nextLine().toLowerCase();
+        while (!reply.equals("yes") && !reply.equals("y")) {
+            System.out.print("Previous result deleted. Please re-enter your name: ");
+            name = scan.nextLine();
+            System.out.println("Hello " + name + ". Is this correct?");
+            reply = scan.nextLine().toLowerCase();
+        }
+
     }
 }
