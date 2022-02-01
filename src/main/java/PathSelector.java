c1f3181bfe9e0db383d6ce3af1ba8712982e848f
shittake <ericleowyuquan@gmail.com>
2022-02-01 21:29:39 +0800

    Allow user to input path choice with preliminary input validation. More paths will be added in the future with further elaboration.

--- src/main/java/PathSelector.java
+++ src/main/java/PathSelector.java
@@ -1,13 +1,24 @@
 package main.java;
+import java.util.Scanner;
 
 public class PathSelector {
 
     private String name;
-    public PathSelector(String name) {
+    private Scanner scan;
+    public PathSelector(String name, Scanner scan) {
         this.name = name;
+        this.scan = scan;
     }
 
     public void run() {
         System.out.println("Hi " + this.name);
+        System.out.println("Which path would you like to choose?");
+
+        String reply = scan.nextLine();
+        while (!reply.equals("A") && !reply.equals("B")) {
+            System.out.println("Invalid Command. Type again.");
+            reply = scan.nextLine();
+        }
+        System.out.println("Very well! You chose " + reply);
     }
 }
