2178cfb84c5fd140df9db98447fa3d0f77c5075f
shittake <ericleowyuquan@gmail.com>
2022-02-07 17:37:35 +1100

    Update TimeOutOfBounds class with custom error message.

--- src/main/java/TimeOutOfBounds.java
+++ src/main/java/TimeOutOfBounds.java
@@ -1,2 +1,13 @@
-package main.java;public class TimeOutOfBounds {
+package main.java;
+
+public class TimeOutOfBounds extends InvalidInput{
+
+    public TimeOutOfBounds() {
+    }
+
+    @Override
+    public String toString() {
+        String line = "Time does not make sense! ";
+        return line + super.toString();
+    }
 }
