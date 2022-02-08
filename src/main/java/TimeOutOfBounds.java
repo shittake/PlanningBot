766537e971b450dbcfb69c07e4d7483560ec54f9
shittake <ericleowyuquan@gmail.com>
2022-02-08 21:20:31 +1100

    Add minor formatting changes and comments

--- src/main/java/TimeOutOfBounds.java
+++ src/main/java/TimeOutOfBounds.java
@@ -1,9 +1,10 @@
 package main.java;
 
+// This is a specific error class that is raised when the time provided is outside a normal range
+// For example, providing any time other than midnight/noon/12am to 11.59am, 12pm to 11.59pm will raise this error
 public class TimeOutOfBounds extends InvalidInput{
 
-    public TimeOutOfBounds() {
-    }
+    public TimeOutOfBounds() {}
 
     @Override
     public String toString() {
