e9d85b5e84bf2918e55710f0fff93f55e9535125
shittake <ericleowyuquan@gmail.com>
2022-02-10 14:45:03 +1100

    Add additional information mentioning the different paths available for the user to choose from. Further abstraction is done by calling the InvalidInput class in the PathSelector class

--- src/main/java/PathSelector.java
+++ src/main/java/PathSelector.java
@@ -13,10 +13,13 @@ public class PathSelector {
     public void run() {
         System.out.println("Hi " + this.name);
         System.out.println("Which path would you like to choose?");
+        System.out.println("Path A: You have some events happening and some locations available. You wish to find an optimal way to schedule the events.");
+        System.out.println("Path B: You have some items and some people who have stated their preferences for the items. You wish to find an optimal way to distribute the items.");
 
         String reply = scan.nextLine();
         while (!reply.equals("A") && !reply.equals("B")) {
-            System.out.println("Invalid Command. Type again.");
+            InvalidInput error = new InvalidInput();
+            System.out.println(error);
             reply = scan.nextLine();
         }
         System.out.println("Very well! You chose " + reply);
