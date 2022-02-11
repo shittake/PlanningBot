5a9c0f57bc7bf5f0da8f33bfc6da9d7036a17419
shittake <ericleowyuquan@gmail.com>
2022-02-11 21:33:44 +1100

    Create new Path class for all possible paths chosen by the user

---
+++ src/main/java/Path.java
@@ -0,0 +1,15 @@
+package main.java;
+
+public class Path {
+
+    private String name;
+
+    public Path(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Path ";
+    }
+}
