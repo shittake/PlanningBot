04e6f0abca5ce8e0d8f67d1bc02a8bc43ce5ffa2
shittake <ericleowyuquan@gmail.com>
2022-02-11 21:31:39 +1100

    Create separate paths for Path A and Path B

--- src/main/java/PathSelector.java
+++ src/main/java/PathSelector.java
@@ -15,7 +15,7 @@ public class PathSelector {
         System.out.println("Which path would you like to choose?");
         System.out.println("Path A: You have some events happening and some locations available. You wish to find an optimal way to schedule the events.");
         System.out.println("Path B: You have some items and some people who have stated their preferences for the items. You wish to find an optimal way to distribute the items.");
-
+        System.out.println("This is caps sensitive!");
         String reply = scan.nextLine();
         while (!reply.equals("A") && !reply.equals("B")) {
             InvalidInput error = new InvalidInput();
@@ -23,5 +23,18 @@ public class PathSelector {
             reply = scan.nextLine();
         }
         System.out.println("Very well! You chose " + reply);
+        if (reply.equals("A")) {
+            moveA();
+        } else {
+            moveB();
+        }
+    }
+
+    public void moveA() {
+        pass;
+    }
+
+    public void moveB() {
+        pass;
     }
 }
