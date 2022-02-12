1695f7361acf9022ccb5da5c4a0bf2c29e1d60c9
shittake <ericleowyuquan@gmail.com>
2022-02-12 23:05:42 +1100

    Add PathA class that will find an optimal way to arrange events such that the most number of events can be carried out in various locations without clashes

---
+++ src/main/java/PathA.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class PathA extends Path {
+
+    public PathA(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "A";
+    }
+}
