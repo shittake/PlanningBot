0bafba6290788f09064a74bec18e009af9f1dd82
shittake <ericleowyuquan@gmail.com>
2022-02-14 23:31:25 +1100

    Update Path class to take in Scanner as another input

--- src/main/java/Path.java
+++ src/main/java/Path.java
@@ -1,13 +1,23 @@
 package main.java;
+import java.util.Scanner;
 
 public class Path {
 
     private String name;
+    private Scanner scan;
 
-    public Path(String name) {
+    public Path(String name, Scanner scan) {
         this.name = name;
+        this.scan = scan;
     }
 
+    public void run() {
+        System.out.println("Welcome!");
+    }
+
+    public int getInt() {
+        return this.scan.nextInt();
+    }
     @Override
     public String toString() {
         return "Path ";
