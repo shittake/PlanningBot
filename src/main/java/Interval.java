8b6abae745ab053cf8819b0c159b43bce4eec632
shittake <ericleowyuquan@gmail.com>
2022-02-24 23:08:48 +1100

    Create Interval class that stores each Event as an Interval as inputted by the user

---
+++ src/main/java/Interval.java
@@ -0,0 +1,17 @@
+package main.java;
+
+public class Interval {
+
+    private int start, end;
+
+    public Interval(String start, String end) {
+        this.start = convert(start);
+        this.end = convert(end);
+    }
+
+    public int convert(String time) {
+        String[] splitUp = time.split(":");
+        int hour = Integer.parseInt(splitUp[0]);
+        int minute = Integer.parseInt(splitUp[1]);
+    }
+}
