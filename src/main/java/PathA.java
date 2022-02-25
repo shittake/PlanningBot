7475d863843ba9a8eda16da076602b2436d0f80e
shittake <ericleowyuquan@gmail.com>
2022-02-25 11:07:51 +1100

    Add functionality allowing for user to remove any previously-added events

--- src/main/java/PathA.java
+++ src/main/java/PathA.java
@@ -25,7 +25,7 @@ public class PathA extends Path {
     }
 
     public void collectEvents(int answer) {
-        for (int i = 0; i < answer; i++) {
+        for (int i = 0; i < answer; i++) { // modify this to while loop in the future
             // For now, assume all valid inputs
             System.out.println("Please key in the time which Event " + String.valueOf(i+1) + " starts.");
             String startTime = getLine();
@@ -43,6 +43,27 @@ public class PathA extends Path {
         for (int i = 0; i < this.lstOfEvents.size(); i++) {
             System.out.print("Event " + String.valueOf(i+1) + ": ");
             System.out.println(this.lstOfEvents.get(i));
+        }
+        askRemoveEvents();
+    }
+
+    public void askRemoveEvents() {
+        System.out.println("Would you to remove any of the events?");
+        String reply = getLine();
+        int result = Checker.yesNoCheck(reply);
+
+        // Performs input validation and confirmation until user is satisfied
+        while (result != 0) {
+            if (result == 1) {
+                System.out.print("Which event would you like to remove? Type the event number here: ");
+                String toBeRemoved = getLine();
+                int index = Integer.parseInt(toBeRemoved);  // assume this is valid for now
+                System.out.println("Event " + String.valueOf(index) + " to be removed."); // put this into another method
+                this.lstOfEvents.remove(index - 1);
+                System.out.println("Would you still like to remove any events?");
+            }
+            reply = getLine();
+            result = Checker.yesNoCheck(reply);
         }
     }
     @Override
