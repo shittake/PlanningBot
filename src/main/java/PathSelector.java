5376d9bfa574b6f7b61879f7c75613d9f56f0049
shittake <ericleowyuquan@gmail.com>
2022-02-26 17:12:19 +1100

    Modify the expected functionality of each of the Paths so that the purpose of this entire PlanningBot is made more clear-cut and less muddled

--- src/main/java/PathSelector.java
+++ src/main/java/PathSelector.java
@@ -13,8 +13,9 @@ public class PathSelector {
     public void run() {
         System.out.println("Hi " + this.name);
         System.out.println("Which path would you like to choose?");
-        System.out.println("Path A: You have some events happening and some locations available. You wish to find an optimal way to schedule the events.");
-        System.out.println("Path B: You have some items and some people who have stated their preferences for the items. You wish to find an optimal way to distribute the items.");
+        System.out.println("You have some events happening and some locations available. You wish to find an optimal way to schedule the events.");
+        System.out.println("Path A: Events can be conducted back-to-back without any downtime required.");
+        System.out.println("Path B: There is downtime required after each event (cleaning up / rest time) etc.");
         System.out.println("This is caps sensitive!");
         String reply = scan.nextLine();
         while (!reply.equals("A") && !reply.equals("B")) {
