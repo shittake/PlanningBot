7cb1ec9474ce512c79596b48add4391248667eae
shittake <ericleowyuquan@gmail.com>
2022-02-27 23:25:14 +1100

    Modify Interval class to implement Comparable<Interval> so that our balanced BST later on will be able to correctly prioritize the available locations (if any)

--- src/main/java/Interval.java
+++ src/main/java/Interval.java
@@ -1,6 +1,6 @@
 package main.java;
 
-public class Interval {
+public class Interval implements Comparable<Interval> {
 
     private int start, end;
     private String startString, endString;
@@ -22,5 +22,14 @@ public class Interval {
     @Override
     public String toString() {
         return this.startString + " - " + this.endString;
+    }
+
+    @Override
+    public int compareTo(Interval other) {
+        if (other.end < this.end) return 1;
+        else if (other.end > this.end) return 1;
+        else if (other.start > this.start) return 1;
+        else if (other.start < this.start) return -1;
+        else return 0;
     }
 }
