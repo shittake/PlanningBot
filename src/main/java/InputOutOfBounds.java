e1bf57e700a1f25923866fe2c59e79fada135a10
shittake <ericleowyuquan@gmail.com>
2022-03-10 23:59:14 +1100

    Add InputOfBounds class, which is a subclass of InvalidInput, that is raised if the provided input does not fall within the specifications of the program

---
+++ src/main/java/InputOutOfBounds.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class InputOutOfBounds extends InvalidInput {
+
+    @Override
+    public String toString() {
+        return "Input out of bounds!" + super.toString();
+    }
+}
